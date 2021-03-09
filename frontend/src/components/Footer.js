f592495f2ee58c797eae6dc34de1aafadb85be23
Yamendra Pun <punyamendra@gmail.com>
2021-03-09 22:21:08 +0900

    Home screen product listing

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; MyShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; My Shop</Col>
         </Row>
       </Container>
     </footer>
