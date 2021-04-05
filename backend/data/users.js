bede207de00bed8522683a47450b08d882d71b19
Yamendra Pun <punyamendra@gmail.com>
2021-04-05 16:15:15 +0900

    Added Create and Update Product Endpoints

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,18 +4,18 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('12345', 10),
     isAdmin: true,
   },
   {
     name: 'Jon Doe',
     email: 'jon@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('12345', 10),
   },
   {
     name: 'Jane Doe',
     email: 'jane@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('12345', 10),
   },
 ]
 
--- backend/seeder.js
+++ backend/seeder.js
@@ -35,6 +35,7 @@ const importData = async () => {
     process.exit(1)
   }
 }
+
 const destroyData = async () => {
   try {
     await Order.deleteMany()
